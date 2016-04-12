983800d7f5590791d5cd48addfed5136e74bc859
alu0100774054 <alu0100774054@ull.edu.es>
2016-04-12 13:13:21 +0100

    añadido main

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+  public static void main(String[] args) {
+    titulo();
+  }
+
+  private static void titulo() {
+    System.out.println("\nResolución del problema por Bayes.");
+  }
+
+}
